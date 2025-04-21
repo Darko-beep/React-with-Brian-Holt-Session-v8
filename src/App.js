ebe476b196fa3b0333162016eb5cf0568a752ebd
Darko-beep <darkofrederick98@outlook.com>
2025-04-21 03:40:00 +0000

    added props, created a Pet component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement(
         "div",
         {},
-        [
-            React.createElement("h2", {}, "Luna"),
-            React.createElement("h3", {}, "Dog"),
-            React.createElement("h4", {}, "Havanese"),
-        ]
+            [
+            React.createElement("h2", {}, props.name),
+            React.createElement("h3", {}, props.animal),    
+            React.createElement("h4", {}, props.breed),
+            ]
+        
     );
   };
-
 
 const App = () => {
     return React.createElement(
       "div",
       {},
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement("h2", {}, "Luna"),
-      
+      React.createElement(Pet,{
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese",
+      })
     );
-  };
+};
+    
 
-  
+ 
   const container = document.getElementById("root");
   const root = ReactDOM.createRoot(container);
   root.render(React.createElement(App));
