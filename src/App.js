c8d1bef4bd46facbd1be262754cfe28cba249634
Darko-beep <darkofrederick98@outlook.com>
2025-04-21 07:35:43 +0000

    added a package.json, prettier extension

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 const Pet = (props) => {
-    return React.createElement(
-        "div",
-        {},
-            [
-            React.createElement("h2", {}, props.name),
-            React.createElement("h3", {}, props.animal),    
-            React.createElement("h4", {}, props.breed),
-            ]
-        
-    );
-  };
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h4", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet,{
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      })
-    );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+  );
 };
-    
 
- 
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
