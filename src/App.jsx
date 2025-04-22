5ea68117d99f5aaeb2fdebc1835e7fadd523b880
Darko-beep <darkofrederick98@outlook.com>
2025-04-22 19:55:32 +0000

    added new features, components, useState, useEffect and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import {createRoot} from "react-dom/client";
-import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams.jsx";
 
 
 
@@ -8,9 +8,7 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"/>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
-      <Pet name="Doink" animal="Cat" breed="Mixed"/>
+      <SearchParams/>
     </div>
   )
 };
